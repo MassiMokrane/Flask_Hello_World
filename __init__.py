2bc968c705b549b72f981ea2758d1ab1c0fbdcb2
RABAH MOKRANE <97698768+MassiMokrane@users.noreply.github.com>
2025-06-04 16:27:01 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -58,5 +58,5 @@ def cv():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host="0.0.0.0", port=5000)
 
